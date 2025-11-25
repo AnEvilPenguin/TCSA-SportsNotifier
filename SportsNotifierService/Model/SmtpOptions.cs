b7c026851fd0f21daa72055b75a983d26bef01c2
Dave Lewis <siu06dl@gmail.com>
2025-11-25 19:06:13 +0000

    feat: email hello world

--- SportsNotifierService/Model/SmtpOptions.cs
+++ SportsNotifierService/Model/SmtpOptions.cs
@@ -2,7 +2,11 @@ namespace SportsNotifierService.Model;
 
 public record SmtpOptions
 {
-    public required string Server { get; set; }
-    public int Port { get; set; } = 25;
-    public bool EnableSsl { get; set; } = true;
+    public required string Server { get; init; }
+    public int Port { get; init; } = 25;
+    public bool EnableSsl { get; init; } = true;
+    public string? Username { get; init; }
+    public string? Password { get; init; }
+    public required string To { get; init; }
+    public required string From { get; init; }
 };
--- SportsNotifierService/Services/SmtpService.cs
+++ SportsNotifierService/Services/SmtpService.cs
@@ -1,3 +1,4 @@
+using System.Net.Mail;
 using Microsoft.Extensions.Options;
 using SportsNotifierService.Model;
 
@@ -9,6 +10,22 @@ public class SmtpService (IOptions<SmtpOptions> settings) : ISmtpService
     
     public void FakeEmail()
     {
-        Console.WriteLine($"{_settings.Server}:{_settings.Port}");
+        using var mail = new MailMessage();
+        
+        mail.From = new MailAddress(_settings.From);
+        mail.To.Add(new MailAddress(_settings.To));
+        
+        mail.Subject = "Sports Notifier Service";
+        
+        mail.Body = "Hello World!";
+        
+        using var smtp = new SmtpClient(_settings.Server, _settings.Port);
+        
+        if (!string.IsNullOrEmpty(_settings.Username) && !string.IsNullOrEmpty(_settings.Password))
+            smtp.Credentials = new System.Net.NetworkCredential(_settings.Username, _settings.Password);
+        
+        smtp.EnableSsl = _settings.EnableSsl;
+        
+        smtp.Send(mail);
     }
 }
